7fba67bd63190c118c79913b76bb25fca6f89004
Abhijeet Singh <abhi009215@gmail.com>
2024-11-14 11:57:35 +0530

    Create burpXMLparser.sh

---
+++ burpXMLparser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cat extracted.xml | grep "<request base64=" | awk -F'[' '{print $3}' | awk -F']]' '{print $1}' | tee allRequestsInBase64.txt >/dev/null
+
+file=allRequestsInBase64.txt
+
+if [ -e ./requests-from-extractedxml/ ]; then 
+echo "requests-from-extractedxml File exists."
+    else
+    mkdir requests-from-extractedxml 
+fi
+
+if [ -e ./sqlmapLogs/ ]; then 
+echo "sqlmapLogs File exists."
+    else
+    mkdir sqlmapLogs 
+fi
+
+counter=1
+
+for i in `cat $file`
+do
+    echo $i | base64 -d | tee ./requests-from-extractedxml/$counter.txt
+    echo ""
+    echo ""
+    let counter++
+        
+done
+
+rm allRequestsInBase64.txt

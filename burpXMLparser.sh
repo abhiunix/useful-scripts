d0c3aadc896a02e26fbc15c852814a8036ac39bd
Abhijeet Singh <abhi009215@gmail.com>
2024-11-17 10:54:18 +0530

    Update burpXMLparser.sh

--- burpXMLparser.sh
+++ burpXMLparser.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+
 cat extracted.xml | grep "<request base64=" | awk -F'[' '{print $3}' | awk -F']]' '{print $1}' | tee allRequestsInBase64.txt >/dev/null
 
 file=allRequestsInBase64.txt
-
 if [ -e ./requests-from-extractedxml/ ]; then 
-echo "requests-from-extractedxml File exists."
-    else
+    echo "requests-from-extractedxml directory exists."
+else
     mkdir requests-from-extractedxml 
 fi
 
@@ -13,11 +13,26 @@ counter=1
 
 for i in `cat $file`
 do
-    echo $i | base64 -d | tee ./requests-from-extractedxml/$counter.txt
+    decoded_request=$(echo $i | base64 -d)
+    echo "$decoded_request" > ./requests-from-extractedxml/$counter.txt
     echo ""
     echo ""
     let counter++
-        
+done
+
+for file in ./requests-from-extractedxml/*.txt
+do
+    if grep -q "^OPTIONS " "$file"; then
+        echo "Removing $file as it contains an OPTIONS request."
+        rm "$file"
+    fi
+done
+
+counter=1
+for file in ./requests-from-extractedxml/*.txt
+do
+    mv "$file" ./requests-from-extractedxml/$counter.txt
+    let counter++
 done
 
 rm allRequestsInBase64.txt
